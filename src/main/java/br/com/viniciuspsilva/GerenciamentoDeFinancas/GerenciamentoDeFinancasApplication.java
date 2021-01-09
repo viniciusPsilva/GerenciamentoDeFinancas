c1720dfd56e5d3776b16dbd729a7da4adf7d054c
Vinicius Pereira Da Silva <viniciuspsilva@brq.com>
2021-01-08 22:59:16 -0300

    commit inicial

---
+++ src/main/java/br/com/viniciuspsilva/GerenciamentoDeFinancas/GerenciamentoDeFinancasApplication.java
@@ -0,0 +1,13 @@
+package br.com.viniciuspsilva.GerenciamentoDeFinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeFinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeFinancasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/viniciuspsilva/GerenciamentoDeFinancas/GerenciamentoDeFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.viniciuspsilva.GerenciamentoDeFinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
