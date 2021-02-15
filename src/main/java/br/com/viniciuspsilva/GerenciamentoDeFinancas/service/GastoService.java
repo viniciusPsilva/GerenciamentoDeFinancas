2ec8de04afbf99e899543b8bfdf53d5682bc3486
viniciuspsilva <viniciuspsilva1997@gmail.com>
2021-02-15 20:52:46 -0300

    add test controller test

--- src/main/java/br/com/viniciuspsilva/GerenciamentoDeFinancas/service/GastoService.java
+++ src/main/java/br/com/viniciuspsilva/GerenciamentoDeFinancas/service/GastoService.java
@@ -4,6 +4,5 @@ import br.com.viniciuspsilva.GerenciamentoDeFinancas.model.dataContract.GastoDto
 
 public interface GastoService {
     GastoDto cadastrarGasto(GastoDto gasto);
-
     Iterable<GastoDto> listarGastos();
 }
--- src/test/java/br/com/viniciuspsilva/GerenciamentoDeFinancas/gateway/http/controller/GastoControllerTest.java
+++ src/test/java/br/com/viniciuspsilva/GerenciamentoDeFinancas/gateway/http/controller/GastoControllerTest.java
@@ -1,24 +1,35 @@
 package br.com.viniciuspsilva.GerenciamentoDeFinancas.gateway.http.controller;
 
+import br.com.six2six.fixturefactory.Fixture;
+import br.com.six2six.fixturefactory.loader.FixtureFactoryLoader;
 import br.com.viniciuspsilva.GerenciamentoDeFinancas.fixture.GastoFixture;
 import br.com.viniciuspsilva.GerenciamentoDeFinancas.model.dataContract.GastoDto;
 import br.com.viniciuspsilva.GerenciamentoDeFinancas.service.GastoService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.engine.TestExecutionResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import static org.mockito.Mockito.when;
 import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(GastoController.class)
 public class GastoControllerTest {
@@ -32,21 +43,63 @@ public class GastoControllerTest {
     @MockBean
     private GastoService gastoService;
 
-    //TODO ajustar testes
-   // @Test
-    public void deveCadastrarUmGasto() throws Exception {
-        when(gastoService.cadastrarGasto(any(GastoDto.class))).thenReturn(GastoFixture.gastoDto());
-
-        mockMvc.perform(post("/financas/gasto")
-                        .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(GastoFixture.gastoDto()))
-
-        ).andDo(print())
-         .andExpect(status().isCreated());
-
+    @BeforeEach
+    public void setUp() {
+        FixtureFactoryLoader.loadTemplates("br.com.viniciuspsilva.GerenciamentoDeFinancas.template");
     }
 
+
+    @Test
+    public void deveCadastrarUmGasto() throws Exception {
+
+        GastoDto gastoDto = Fixture.from(GastoDto.class).gimme("gasto");
+
+        when(gastoService.cadastrarGasto(any(GastoDto.class))).thenReturn(gastoDto);
+
+        MvcResult mvcResult = mockMvc.perform(post("/financas/gasto")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(gastoDto))
+
+        ).andReturn();
+
+        MockHttpServletResponse response = mvcResult.getResponse();
+        Assertions.assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+    }
+
+    @Test
+    public void deveListarGastos() throws Exception {
+
+        GastoDto gastoDto = Fixture.from(GastoDto.class).gimme("gasto");
+
+        when(gastoService.listarGastos()).thenReturn(List.of(gastoDto));
+
+        MvcResult mvcResult = mockMvc.perform(get("/financas/gasto")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn();
+
+        MockHttpServletResponse response = mvcResult.getResponse();
+        String responseBody = response.getContentAsString();
+        GastoDto responseObject = objectMapper.readValue(responseBody, new TypeReference<List<GastoDto>>(){}).get(0);
+
+        Assertions.assertEquals(HttpStatus.OK.value(), response.getStatus());
+        Assertions.assertEquals(gastoDto.getId(), responseObject.getId());
+        Assertions.assertEquals(gastoDto.getNome(), responseObject.getNome());
+        Assertions.assertEquals(gastoDto.getDescricao(), responseObject.getDescricao());
+        Assertions.assertEquals(gastoDto.getStatus(), responseObject.getStatus());
+        Assertions.assertEquals(gastoDto.getIdPlanoDeGasto(),responseObject.getIdPlanoDeGasto());
+        Assertions.assertEquals(gastoDto.getIdCategoria(),responseObject.getIdCategoria());
+        Assertions.assertEquals(gastoDto.getDataCriacao(),responseObject.getDataCriacao());
+        Assertions.assertEquals(gastoDto.getDataReferencia(),responseObject.getDataReferencia());
+        Assertions.assertEquals(gastoDto.getDataVencimento(),responseObject.getDataVencimento());
+        Assertions.assertEquals(gastoDto.getValor(),responseObject.getValor());
+        Assertions.assertEquals(gastoDto.getParcelaAtual(),responseObject.getParcelaAtual());
+        Assertions.assertEquals(gastoDto.getTotalParcelas(),responseObject.getTotalParcelas());
+        Assertions.assertEquals(gastoDto.getTipo(),responseObject.getTipo());
+
+
+    }
 
 
 }
