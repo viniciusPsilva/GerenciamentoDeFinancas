a64d99f97ad11e2cea67038cbdd27783d499a0a4
vini <viniciuspsilva1997@gmail.com>
2021-09-02 21:07:06 -0300

    ajuste CategoriaServiceImpl

---
+++ src/main/java/br/com/viniciuspsilva/GerenciamentoDeFinancas/exception/categoria/CategoriaException.java
@@ -0,0 +1,8 @@
+package br.com.viniciuspsilva.GerenciamentoDeFinancas.exception.categoria;
+
+public class CategoriaException extends RuntimeException{
+
+    public CategoriaException(String msg) {
+        super(msg);
+    }
+}
--- src/main/java/br/com/viniciuspsilva/GerenciamentoDeFinancas/service/impl/CategoriaServiceImpl.java
+++ src/main/java/br/com/viniciuspsilva/GerenciamentoDeFinancas/service/impl/CategoriaServiceImpl.java
@@ -1,11 +1,16 @@
 package br.com.viniciuspsilva.GerenciamentoDeFinancas.service.impl;
 
+import br.com.viniciuspsilva.GerenciamentoDeFinancas.exception.categoria.CategoriaException;
 import br.com.viniciuspsilva.GerenciamentoDeFinancas.exception.categoria.CategoriaNotFoundException;
 import br.com.viniciuspsilva.GerenciamentoDeFinancas.gateway.repository.CategoriaRepository;
 import br.com.viniciuspsilva.GerenciamentoDeFinancas.model.entities.Categoria;
 import br.com.viniciuspsilva.GerenciamentoDeFinancas.service.CategoriaService;
+import net.bytebuddy.implementation.bytecode.Throw;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.logging.Logger;
 
 @Service
 public class CategoriaServiceImpl implements CategoriaService {
@@ -16,11 +21,21 @@ public class CategoriaServiceImpl implements CategoriaService {
     @Override
     public Categoria cadastrar(Categoria categoria)
     {
-        return repository.save(categoria);
+        try {
+            return repository.save(categoria);
+        }catch (Exception ex){
+            throw new CategoriaException("Erro ao cadastrar Categoria");
+        }
     }
 
     @Override
     public Categoria buscar(Integer id) {
-        return repository.findById(id).orElseThrow(() -> new CategoriaNotFoundException("Não foi Possível encontrar a categoria id: " + id));
+        try {
+            return repository.findById(id).orElseThrow(() -> new CategoriaNotFoundException("Não foi Possível encontrar a categoria id: " + id));
+        }catch (CategoriaNotFoundException ex){
+            throw new CategoriaNotFoundException("Não foi Possível encontrar a categoria id: " + id);
+        }catch (Exception ex){
+            throw new CategoriaException("Erro ao buscar categoria por id");
+        }
     }
 }
