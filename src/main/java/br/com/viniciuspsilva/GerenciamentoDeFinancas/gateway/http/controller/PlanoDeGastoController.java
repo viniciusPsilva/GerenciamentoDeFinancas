4eaa8a3bfe00c4b5bc98398bc9cc579442bd5716
vini <vinicius>
2023-06-02 19:56:49 -0300

    em_andamento: adicionando cadastro de planejamento Mensal de gasto.

--- src/main/java/br/com/viniciuspsilva/GerenciamentoDeFinancas/gateway/http/controller/PlanoDeGastoController.java
+++ src/main/java/br/com/viniciuspsilva/GerenciamentoDeFinancas/gateway/http/controller/PlanoDeGastoController.java
@@ -1,19 +1,27 @@
 package br.com.viniciuspsilva.GerenciamentoDeFinancas.gateway.http.controller;
 
 import br.com.viniciuspsilva.GerenciamentoDeFinancas.gateway.repository.PlanoDeGastoRepository;
+import br.com.viniciuspsilva.GerenciamentoDeFinancas.model.dataContract.PlanejamentoMensalDeGastoDto;
+import br.com.viniciuspsilva.GerenciamentoDeFinancas.model.domain.PlanejamentoMensalDeGasto;
 import br.com.viniciuspsilva.GerenciamentoDeFinancas.model.entities.PlanejamentoMensalDeGastoEntity;
+import br.com.viniciuspsilva.GerenciamentoDeFinancas.model.mappers.PlanejamentoMensalDeGastoMapper;
+import br.com.viniciuspsilva.GerenciamentoDeFinancas.service.PlanejamentoMensalDeGastoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/financas/plano")
-@RequiredArgsConstructor
 public class PlanoDeGastoController {
+    @Autowired
+    private PlanoDeGastoRepository repository;
 
-    private final PlanoDeGastoRepository repository;
+    @Autowired
+    private PlanejamentoMensalDeGastoService planejamentoMensalDeGastoService;
 
     @GetMapping
     public ResponseEntity listar(){
@@ -21,6 +29,13 @@ public class PlanoDeGastoController {
         return ResponseEntity.ok(planos);
     }
 
+    @PostMapping
+    public ResponseEntity cadastrar(@RequestBody @Valid PlanejamentoMensalDeGastoDto planoDto){
+        final PlanejamentoMensalDeGasto planejamentoMensalDeGasto = PlanejamentoMensalDeGastoMapper.INSTANCE.mapFromPladoDeGastoDto(planoDto);
 
+        PlanejamentoMensalDeGasto planejamentoCadastrado = planejamentoMensalDeGastoService.cadastrar(planejamentoMensalDeGasto);
+
+        return ResponseEntity.created(URI.create("/financas/plano/"+ planejamentoCadastrado.getId())).build();
+    }
 
 }
--- src/main/java/br/com/viniciuspsilva/GerenciamentoDeFinancas/service/PlanejamentoMensalDeGastoService.java
+++ src/main/java/br/com/viniciuspsilva/GerenciamentoDeFinancas/service/PlanejamentoMensalDeGastoService.java
@@ -1,9 +1,11 @@
 package br.com.viniciuspsilva.GerenciamentoDeFinancas.service;
 
+import br.com.viniciuspsilva.GerenciamentoDeFinancas.model.domain.PlanejamentoMensalDeGasto;
 import br.com.viniciuspsilva.GerenciamentoDeFinancas.model.entities.PlanejamentoMensalDeGastoEntity;
 
 public interface PlanejamentoMensalDeGastoService {
 
     PlanejamentoMensalDeGastoEntity buscar(Integer id);
+    PlanejamentoMensalDeGasto cadastrar(PlanejamentoMensalDeGasto plano);
 
 }
